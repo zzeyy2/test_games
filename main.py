c20725c88eaca9d4a46b590890dad4bd053a62c9
zzeyy <115314211+zzeyy2@users.noreply.github.com>
2023-08-31 23:24:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -25,16 +25,17 @@ async def manager(db: GameDB):
 
         os.system('cls||clear')
 
-        if any((title, publisher, year)):
+        if not any((title, publisher, year)):
+            print('Вы не ввели никаких данных!')
+            return
         
-            if await db.add(title, publisher, year):
-                print('Данные успешно добавлены!')
-
-            else:
-                print('Игра уже добавлена!')
+        if await db.add(title, publisher, year):
+            print('Данные успешно добавлены!')
 
         else:
-            print('Вы не ввели никаких данных!')
+            print('Игра уже добавлена!')
+
+        
 
     elif '2' in choice:
         print(f'Если хотите пропустить шаг - оставьте поле пустым')
@@ -46,22 +47,23 @@ async def manager(db: GameDB):
 
         os.system('cls||clear')
 
-        if any((title, publisher, year)):
-        
-            games = await db.search(title, publisher, year)
-
-            if games:
-                print(f'Название|Издатель|Год')
-
-                for game in games:
-                    game = [str(_) for _ in game]
-
-                    print('|'.join(game))
-            else:
-                print('Игра не найдена!')
-
-        else:
+        if not any((title, publisher, year)):
             print('Вы не ввели никаких данных!')
+            return
+        
+        games = await db.search(title, publisher, year)
+
+        if games:
+            print(f'Название|Издатель|Год')
+
+            for game in games:
+                game = [str(_) for _ in game]
+
+                print('|'.join(game))
+        else:
+            print('Игра не найдена!')
+
+        
 
 
     elif '3' in choice:
@@ -73,15 +75,17 @@ async def manager(db: GameDB):
         
         os.system('cls||clear')
 
-        if any((title, publisher, year)):
-
-            if await db.remove(title, publisher, year):
-                print('Игра успешно удалена!')
-            else:
-                print('Игра не найдена!')
-
-        else:
+        if not any((title, publisher, year)):
             print('Вы не ввели никаких данных!')
+            return
+        
+        if await db.remove(title, publisher, year):
+            print('Игра успешно удалена!')
+        else:
+            print('Игра не найдена!')
+
+        
+            
 
     elif '4' in choice:
         print(f'Если хотите пропустить шаг - оставьте поле пустым')
@@ -91,7 +95,13 @@ async def manager(db: GameDB):
         old_year=is_digit_or_none(input(f'Введите старый год издания игры:\n'))
 
         if not any((old_title, old_publisher, old_year)):
+            os.system('cls||clear')
             print('Вы не ввели данных необходимых для поиска\nПопробуйте еще раз')
+            return
+        
+        if not await db.search(old_title, old_publisher, old_year):
+            os.system('cls||clear')
+            print('Игра не найдена!')
             return
         
 
@@ -115,26 +125,32 @@ async def manager(db: GameDB):
 
     elif '5' in choice:
         games = await db.get_all()
-        if games:
 
-            print(f'Название|Издатель|Год')
-            for game in games:
-                game = [str(_) for _ in game]
-                
-                        
-                print('|'.join(game))
-
-        else:
+        if not games:
+            os.system('cls||clear')
             print("База данных пустая. Вам нужно добавить игры,\nчто бы пользоваться этой функцией")
+            return
+        
+        os.system('cls||clear')
+        print(f'Название|Издатель|Год')
+        for game in games:
+            game = [str(_) for _ in game]
+                
+            print('|'.join(game))
+
+
+            
 
 async def main():
     db = GameDB(r'core/database/db.db')
-    await db.initialize()
+    await db._initialize()
+
     while True:
         await asyncio.create_task(manager(
             db=db
         ))
         
+            
 
 
 
